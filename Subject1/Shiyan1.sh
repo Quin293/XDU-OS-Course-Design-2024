633129de35e7c6b88a53b57873c2e9dce0e20d89
大秦帝锅 <47657507+Quin293@users.noreply.github.com>
2024-04-12 23:05:34 +0800

    Add files via upload

---
+++ Subject1/Shiyan1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#检查格式
+if [ $# -ne 2 ]; then
+	echo "FORMAT ERROR"
+	exit 1
+fi
+
+#定义输入参数
+file=$1
+flag=$2
+
+#生成shell脚本
+echo "#!/bin/bash" > NewShell.sh
+echo "if [ \"$flag\" = \"read\" ]; then" >> NewShell.sh
+echo "	cat \"$file\"" >> NewShell.sh
+echo "fi" >> NewShell.sh
+echo "if [ \"$flag\" = \"write\" ]; then" >> NewShell.sh
+
+#输入要读入的参数
+echo "	read -p \"Enter content to write\" content" >> NewShell.sh
+echo "	echo \"\$content\" >> \"$file\"" >> NewShell.sh
+echo "fi" >> NewShell.sh
+
+#将新脚本设为可执行
+chmod +x NewShell.sh
+
+#使用strace观察执行结果
+strace -o output.txt ./NewShell.sh "$file" "$flag"
+
+#输出结果
+echo "Strace Output:"
+cat output.txt
+
+#删除新文件以便再次执行
+rm NewShell.sh output.txt
